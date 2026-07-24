7314673476001de6ad00f40b4cdc5f082e4e919e
elghandori1 <mohamedelghandori4@gmail.com>
2026-07-25 00:21:54 +0100

    fix warnning PageSpeed Insights navbar links color

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -19,7 +19,6 @@ type NavLinksProps = {
 export default function NavLinks({ locale, t }: NavLinksProps) {
   const pathname = usePathname();
 
-  // Helper to check if a path is active
   const isActive = (path: string) => {
     const fullPath = `/${locale}${path}`;
     return pathname === fullPath;
@@ -34,25 +33,27 @@ export default function NavLinks({ locale, t }: NavLinksProps) {
   ];
 
   return (
-   <div dir={locale === "ar" ? "rtl" : "ltr"}  className="hidden md:flex items-center gap-8 text-sm font-medium">
-  {navItems.map((item) => {
-    const fullHref = `/${locale}${item.href}`;
-    const active = isActive(item.href);
-    return (
+    <div
+      dir={locale === "ar" ? "rtl" : "ltr"}
+      className="hidden md:flex items-center gap-8 text-sm font-medium"
+    >
+      {navItems.map((item) => {
+        const fullHref = `/${locale}${item.href}`;
+        const active = isActive(item.href);
+        return (
       <Link
-        key={item.href}
-        href={fullHref}
-        className={`transition-colors relative hover:text-blue-500 dark:hover:text-blue-400 ${
-          active ? "text-blue-500 dark:text-blue-400" : "text-foreground"
-        }`}
-      >
-        {item.label}
-        {active && (
-          <span className="absolute -bottom-1 left-0 w-full h-0.5 bg-blue-500 dark:bg-blue-400 rounded-full" />
-        )}
-      </Link>
-    );
-  })}
-</div>
+            key={item.href}
+            href={fullHref}
+            className={`transition-colors relative hover:text-blue-600 dark:hover:text-blue-400 ${
+              active ? "text-blue-600 dark:text-blue-400" : "text-foreground"
+            }`}
+          >
+            {item.label}
+            {active && (
+<span className="absolute -bottom-1 left-0 w-full h-0.5 bg-blue-600 dark:bg-blue-400 rounded-full" />            )}
+          </Link>
+        );
+      })}
+    </div>
   );
 }
